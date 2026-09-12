4a8d74cd40afc530f631c77f0c84b728ca656a84
SumDumIdiut <oliverprakke@gmail.com>
2026-09-12 21:24:54 +0200

    Bump DOTnet (recharge.multiplayer) to 1.7.9
    Reflects tonight's save-corruption fix, Hide & Seek/Infection removal,
    and the file reorg - the version field hadn't moved since 1.7.8.

--- recharge-multiplayer/RechargeMultiplayerMod.cs
+++ recharge-multiplayer/RechargeMultiplayerMod.cs
@@ -5,7 +5,7 @@ public class RechargeMultiplayerMod : IRechargeMod
 {
     public string Id => "recharge.multiplayer";
     public string DisplayName => "DOTnet";
-    public Version Version => new Version(1, 7, 8);
+    public Version Version => new Version(1, 7, 9);
 
     public void OnLoad(IRechargeHost host)
     {
